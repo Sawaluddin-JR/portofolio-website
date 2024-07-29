f3892ad518d64bf1c4f0c0a189846ff2358b09bb
Sawaluddin <sawaluddinsrg472@gmail.com>
2024-07-30 02:18:46 +0700

    View Header is Problem

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import "./header.css";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="#home" className="nav__logo">Smith</a>
+        <div className={`nav__menu ${isOpen ? 'show-menu' : ''}`}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+          {/* <i className="uil uil-times nav__close"></i> */}
+          <i className="uil uil-times nav__close" onClick={toggleMenu}></i>
+        </div>
+        {/* <div className="nav__toggle">
+          <i className="uil uil-apps"></i>
+        </div> */}
+        <div className="nav__btns">
+          {/* <i className="uil uil-moon change-theme" id="theme-button"></i> */}
+          <div className="nav__toggle" onClick={toggleMenu}>
+            <i className="uil uil-apps"></i>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
